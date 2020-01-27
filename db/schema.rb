a65e2ed4951b921820810002269b3f942e4a40ff
rypatel126 <rypatel126@gmail.com>
2020-01-27 13:51:15 -0500

    Changed cities_zip data type from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_183105) do
+ActiveRecord::Schema.define(version: 2020_01_27_184842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string "city_name"
-    t.integer "city_zip"
+    t.string "city_zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
